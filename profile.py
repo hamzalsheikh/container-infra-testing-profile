fa764e45be33b08720a5c1f8ee63ea8ef956e97a
hamzalsheikh <7.amza98@gmail.com>
2023-10-04 12:57:45 -0600

    testing parameters again

--- profile.py
+++ profile.py
@@ -8,17 +8,36 @@ Wait for the profile instance to start, then click on the node in the topology a
 
 # Import the Portal object.
 import geni.portal as portal
+
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
 
 # Create a portal context.
 pc = portal.Context()
 
+# Describe the parameter(s) this profile script can accept.
+pc.defineParameter("dataset", "Dataset to use", portal.ParameterType.STRING, 1)
+
+# Retrieve the values the user specifies during instantiation.
+params = pc.bindParameters()
+
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
 node = request.RawPC("node")
+
+# Check parameter validity. Should fetch the dataset from the long-term volume
+match params.dataset:
+    case "sandy":
+        
+        pc.reportError( portal.ParameterError( "Sandy dataset is not available right now") )
+
+    case "snow":
+        pc.reportError( portal.ParameterError( "Snow dataset is not available right now") )
+    
+    case _:
+        pc.reportError( portal.ParameterError( "You must choose one of the available dataset") )
 
 # Allocate a node and ask for a 30GB file system mounted at /mydata
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD"
@@ -27,25 +46,6 @@ bs.size = "30GB"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-
-
-# TESTING SETTING PARAMETERS
-
-# Describe the parameter(s) this profile script can accept.
-portal.context.defineParameter( "n", "Number of VMs", portal.ParameterType.INTEGER, 1 )
-
-# Retrieve the values the user specifies during instantiation.
-params = portal.context.bindParameters()
-
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
-
-# Check parameter validity.
-if params.n < 1 or params.n > 8:
-    portal.context.reportError( portal.ParameterError( "You must choose at least 1 and no more than 8 VMs.", ["n"] ) )
-
-# Abort execution if there are any errors, and report them.
-portal.context.verifyParameters()
 
 
 # Print the RSpec to the enclosing page.
