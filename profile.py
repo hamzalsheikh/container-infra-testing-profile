fd4e83cdb588710ec28cab966bd4cfea266a6610
hamzalsheikh <7.amza98@gmail.com>
2023-10-04 11:54:00 -0600

    trying parameters

--- profile.py
+++ profile.py
@@ -28,5 +28,25 @@ bs.size = "30GB"
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 
+
+# TESTING SETTING PARAMETERS
+
+# Describe the parameter(s) this profile script can accept.
+portal.context.defineParameter( "n", "Number of VMs", portal.ParameterType.INTEGER, 1 )
+
+# Retrieve the values the user specifies during instantiation.
+params = portal.context.bindParameters()
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+
+# Check parameter validity.
+if params.n < 1 or params.n > 8:
+    portal.context.reportError( portal.ParameterError( "You must choose at least 1 and no more than 8 VMs.", ["n"] ) )
+
+# Abort execution if there are any errors, and report them.
+portal.context.verifyParameters()
+
+
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
